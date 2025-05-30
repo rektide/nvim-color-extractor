bf11aa0994ebd802fbe0f05f73de4d1d310574eb
rektide de la faye <rektide@voodoowarez.com>
2025-05-29 22:04:18 -0400

    fix src/index.ts again

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,7 @@
 #!/usr/bin/env -S npx ts-node
 
 import { run } from "@oclif/core"
-import { Command } from "@oclif/core"
-import { ListColorschemes } from "./commands/list-colorschemes"
-import { ParseTheme } from "./commands/parse-theme"
-import { ExtractColors } from "./commands/extract-colors"
-import { ExtractAllColorschemes } from "./commands/extract-all-colorschemes"
-import { ToGhost } from "./commands/to-ghost"
 
-async function main() {
-  try {
-    const result = await run()
-    if (result === undefined) {
-      // Show help if no command specified
-      await Command.run(["--help"])
-    }
-    process.exit(0)
-  } catch (error) {
-    console.error(error)
-    process.exit(1)
-  }
-}
-
-main()
+run()
+	.then(() => { })
+	.catch(console.error)
